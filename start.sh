50c02d655d183877608344a6eb99abb3d003466f
TELLYHUBCLOUD <147921265+TELLYHUBCLOUD@users.noreply.github.com>
2023-12-01 14:20:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TELLYHUBCLOUD/iTSOK_BOT-2.git /PiroAutoFilterBot
+  git clone https://github.com/TELLYHUBCLOUD/iTSOK_BOT-2.git /iTSOK_BOT-2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /iTSOK_BOT-2
